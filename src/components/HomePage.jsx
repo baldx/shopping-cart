0b1b0c441b67d55a958143c1f6fec4dc0163a084
baldx <mr.ahmad.m2006@gmail.com>
2024-03-08 21:12:56 +0100

    remove fetch from homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react"
-
 function HomePage () {
-    const [mainImg, setMainImg] = useState('');
-
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>{
-                console.log(json)
-                setMainImg(json[1].image)
-            })
-
-
-    }, []);
 
     return (
         <>
-            <img src={mainImg} className="head-image" />
+            <img src="./src/assets/homepage-bg-img.jpg" className="head-image" />
+            <a href="#" className="shop-btn">
+                <button>Shop now</button>
+            </a>
         </>
     )
 }
