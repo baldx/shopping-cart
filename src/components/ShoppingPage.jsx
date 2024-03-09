4f1b8b5833a35a8511c9a1bcaa2c647a60ae821d
baldx <mr.ahmad.m2006@gmail.com>
2024-03-09 23:40:22 +0100

    remove fetching

--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -1,52 +1,18 @@
 import Header from "./Header";
 import Card from "./Card";
-import { useEffect, useState } from "react";
-
-const useImageURL = () => {
-    const [images, setImages] = useState([]);
-    const [error, setError] = useState(null); 
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        let mounted = true;
-
-        fetch('https://fakestoreapi.com/products', {mode: "cors"})
-            .then(response => {
-                if (response.status >= 400 || !response.ok) throw new Error("server Error");
-                return response.json()
-            })
-            .then(json => {
-                if (mounted) {
-                    const imageURLs = json.map(product => product.image);
-                    setImages(prevImages => [...prevImages, ...imageURLs]);
-                }
-            })
-            .catch(error => {
-                if (mounted) {
-                    setError(error)
-                }
-            })
-            .finally(() => setLoading(false))
-
-        return () => {
-            mounted = false;
-        };
-    }, []);
-
-    return { images, error, loading };
-}
+import useImageURL from "./Fetching";
 
 function ShoppingPage () {
 
     const { images, error, loading } = useImageURL();
 
-    if (error) return <p>A network error was encountered</p> //adds error during error
-    if (loading) return <p>Loading...</p> //adds loading during loading
+    if (error) return <p>A network error was encountered</p> //CHANGE IT LATER
+    if (loading) return <p>Loading...</p> //CHANGE IT LATER
 
     function createCards () {
         let result = [];
         for (let index = 0; index < images.length; index++) {
-            result.push(<Card image={images[index]} key={index}/>)
+            result.push(<Card image={images[index]} key={index} index={index} />)
         }
         return result
     }
@@ -56,10 +22,10 @@ function ShoppingPage () {
         <Header />
 
         <main className="shopping-main">
-            {createCards()}
+        {createCards()}
         </main>
         </>
-    )
+    );
 }
 
 export default ShoppingPage;
