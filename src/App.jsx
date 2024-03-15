e54c9edda49e79662a483a03718a9613d00d3c08
baldx <mr.ahmad.m2006@gmail.com>
2024-03-15 21:06:09 +0100

    add history to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
+import PurchaseScreen from './components/PurchaseScreen';
+import Cart from './components/Cart';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const [hide, setHide] = useState(false)
+
+  const addToCart = (product, amount) => {
+    console.log("rsra");
+    setCartItems([...cartItems, { product, amount }]);
+  }
+
+  const Show = () => {
+    setHide(true);
+  }
+
   return (
     <>
-      <Header />
-      <main className="homepage-main">
-        <HomePage/>
-      </main>
+      <Header onClick={Show}/>
+
+      <HomePage/>
+      {hide && <PurchaseScreen addToCart={addToCart}/>}
+      {hide && <Cart cartItems={cartItems} />}
     </>
   )
 }
