fcb662e8cfd5df17b302f46c2bfb69a11fb14c3a
baldx <mr.ahmad.m2006@gmail.com>
2024-03-15 22:01:56 +0100

    move functions

--- src/components/PurchaseScreen.jsx
+++ src/components/PurchaseScreen.jsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-import useImageURL from "./Fetching";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 import Header from "./Header";
 
-
 // eslint-disable-next-line react/prop-types
-function PurchaseScreen ({ addToCart }) {
-    const { data, loading, error } = useImageURL();
-    const { index } = useParams();
-    const [product, setProduct] = useState(null);
+function PurchaseScreen ({ addToCart, product }) {
     const [amount, setAmount] = useState(1);
-
-    useEffect(() => {
-        if (data && data[index]) {
-            setProduct(data[index]);
-        }
-    }, [data, index]); //try moving useEffect to app.jsx, to parent component and see if it works. use useState to link between the 2 components
-
 
     const increaseProducts = () => {
         setAmount(e => e + 1);
@@ -26,27 +13,22 @@ function PurchaseScreen ({ addToCart }) {
         setAmount(e => Math.max(e - 1, 1));
     }
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    if (!product) return null;
-
     const handlePurchase = () => {
-        addToCart = [product, amount]
-        console.log(addToCart, "purchaseScreen.jsx");
-        return addToCart
+        console.log(typeof addToCart); 
+        addToCart("hello")
     }
 
     return (
         <>
             <Header goBack={true}/>
             <main className="purchase-main">
-                <img src={product.image} className="purchase-card-img" />
+                <img src={product} className="purchase-card-img" />
 
                 <div className="product-actions">
                     <div className="top">
-                        <p className="product-title">{product.title}</p>
-                        <p className="description">{product.description}</p>
-                        <p className="price">{Math.round(product.price * amount)}$</p>
+                        <p className="product-title">{product}</p>
+                        <p className="description">{product}</p>
+                        <p className="price">{Math.round(product * amount)}$</p>
                     </div>
 
                     <div className="bottom">
