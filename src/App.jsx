4bc663d186f20cfc971f64a58df6304bc02a3cd7
baldx <mr.ahmad.m2006@gmail.com>
2024-03-15 22:01:45 +0100

    move functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
 import PurchaseScreen from './components/PurchaseScreen';
-import Cart from './components/Cart';
+import Cart from './components/Cart'
+import { useParams } from 'react-router-dom';
+import useImageURL from './components/Fetching';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
   const [hide, setHide] = useState(false)
+  const { data, loading, error } = useImageURL();
+  const { index } = useParams();
+  const [product, setProduct] = useState(null);
 
-  const addToCart = (product, amount) => {
-    console.log("rsra");
-    setCartItems([...cartItems, { product, amount }]);
+  useEffect(() => {
+    if (data && data[index]) {
+        setProduct(data[index]);
+    }
+  }, [data, index]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    if (!product) return null;
+
+  const addToCart = (product) => {
+    console.log("Adding to cart:", product);
+   return product;
+  }
+
+  const Debug = () => {
+    console.log(product);
   }
 
   const Show = () => {
     setHide(true);
+  }
+
+  const returnProduct = () => {
+    console.log(product);
+    return product;
   }
 
   return (
@@ -23,7 +47,7 @@ function App() {
       <Header onClick={Show}/>
 
       <HomePage/>
-      {hide && <PurchaseScreen addToCart={addToCart}/>}
+      {hide && <PurchaseScreen addToCart={addToCart} product={product}/>}
       {hide && <Cart cartItems={cartItems} />}
     </>
   )
