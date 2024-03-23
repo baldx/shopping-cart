f171a3472f66496ff3ed66e5f08d64cfe38584b5
baldx <mr.ahmad.m2006@gmail.com>
2024-03-23 21:33:50 +0100

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer () {
+    return (
+        <>
+            <footer>
+                <p className="top-text">Welcome to our shopping cart project! This project is developed as an assignment from The Odin Project course and also serves as a showcase piece for my portfolio. It utilizes the Fakestore API, React, PropTypes, routing packages, and Vite template to offer you an efficient and user-friendly shopping experience.</p>
+                <p className="bottom-text">Explore our site, browse through our collection, and enjoy the seamless shopping journey we&apos;ve crafted just for you!</p>
+            </footer>
+        </>
+    )
+}
