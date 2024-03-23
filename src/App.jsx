c9195896256b66f212d7fd3ea66844cf14657c67
baldx <mr.ahmad.m2006@gmail.com>
2024-03-23 16:01:42 +0100

    remove logic & functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,41 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
-import PurchaseScreen from './components/PurchaseScreen';
-import Cart from './components/Cart'
-import { useParams } from 'react-router-dom';
-import useImageURL from './components/Fetching';
+
+
+
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const [hide, setHide] = useState(false)
-  const { data, loading, error } = useImageURL();
-  const { index } = useParams();
-  const [product, setProduct] = useState(null);
 
-  useEffect(() => {
-    if (data && data[index]) {
-        setProduct(data[index]);
-    }
-  }, [data, index]);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    if (!product) return null;
 
-  const addToCart = (product) => {
-    console.log("Adding to cart:", product);
-   return product;
-  }
+  // useEffect(() => {
+  //   if (data && data[index]) {
+  //       setProduct(data[index]);
+  //   }
+  // }, [data, index]);
 
-  const Debug = () => {
-    console.log(product);
-  }
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
+  // if (!product) return console.log("product failed to fetch");
 
-  const Show = () => {
-    setHide(true);
-  }
-
-  const returnProduct = () => {
-    console.log(product);
-    return product;
-  }
 
   return (
     <>
-      <Header onClick={Show}/>
-
-      <HomePage/>
-      {hide && <PurchaseScreen addToCart={addToCart} product={product}/>}
-      {hide && <Cart cartItems={cartItems} />}
+      <Header />
+      <HomePage />
     </>
   )
+
+  // return (
+  //   <>
+  //     <Shop.Provider value={{ cartItems, product, setProduct, addToCart }}>
+  //       <Header />
+  //       <HomePage />
+  //       <PurchaseScreen />
+  //     </Shop.Provider>
+  //   </>
+  // )
 }
 
 export default App
