f890aac27139a713e6def48f666a064de38e2c16
baldx <mr.ahmad.m2006@gmail.com>
2024-03-23 16:02:40 +0100

    create Context & add hooks & addCart

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,28 +3,47 @@ import App from "../App";
 import ShoppingPage from "./ShoppingPage";
 import PurchaseScreen from "./PurchaseScreen";
 import Cart from "./Cart";
+import { useState } from "react";
+import useImageURL from "./Fetching";
+import { createContext } from "react";
 
+export const Shop = createContext({});
+  
 const Router = () => {
+    const [cartItems, setCartItems] = useState([]);
+    const { data } = useImageURL();
+    const [product, setProduct] = useState([]);
+    
+    const addToCart = (product, amount) => {
+        console.log(product, amount, "adding to cart");
+        setCartItems([...cartItems, { product, amount }]);
+    }
+    
     const router = createBrowserRouter([
         {
             path: "/",
             element: <App />,
         },
         {
-            path: "shopping",
+            path: "/shopping",
             element: <ShoppingPage />,
         },
         { 
-            path: "shopping/purchase/:index", 
-            element: <PurchaseScreen /> 
+            path: "/shopping/purchase/:index", 
+            element: <PurchaseScreen  data={data}/> 
         },
         {
-            path: "cart",
+            path: "/cart",
             element: <Cart />
         }
     ])
 
-    return <RouterProvider router={router} />;
+    return (
+    <Shop.Provider value={{cartItems, product, setProduct, addToCart}}>
+        <RouterProvider router={router} />
+    </Shop.Provider>
+    
+    )
 }
 
 export default Router;
