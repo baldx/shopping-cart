10b1c2337114954a4407172b1a0cf0b01d6bdde4
baldx <mr.ahmad.m2006@gmail.com>
2024-03-23 20:56:46 +0100

    add cart & logic to displaying products

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
+import { useContext } from 'react';
+import { Shop } from './router';
+import { Link } from 'react-router-dom';
+
 
 export default function Cart ({ cartItems }) {
-
+    const { showShop } = useContext(Shop);
 
     //fix hovering
     //link data coming from PurchaseScreen.jsx to here
@@ -10,18 +15,41 @@ export default function Cart ({ cartItems }) {
     //for ever purchase increase the number of cart
     // finish project :)
 
-    const logStuff = () => {
-        console.log(cartItems + "cart.jsx");
+    function CartValidation (length) {
+        if (length === 0) {
+            return (
+                <>
+                    <Header goBack={showShop}/>
+                    Place your first order!
+                </>
+            )
+        } else return (
+            <>
+                <Header goBack={showShop}/>
+                <main className="main-cart">
+                    <ul>
+                        {cartItems.map((item, index) => (
+                            <li className="item-info" key={index}>
+                                <img className="cart-img" src={item.product.image} />
+                                <div className="item-info-text">
+                                    <div className="item-info-text-child">{item.product.title}</div>
+                                    <div className="item-info-text-child">Amount: {item.amount}</div>
+                                    <div className="item-info-text-child">${Math.round(item.amount * item.product.price)}</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <Link to="/development">
+                        <button className="checkout">Checkout</button>
+                    </Link>
+                </main>
+            </>
+        )
     }
 
     return (
         <>            
-
-            <main className="main-cart">
-                <ul>
-                    <button onClick={logStuff}>dsad</button>
-                </ul>
-            </main>
+            {CartValidation(cartItems.length)}
         </>
     )
 }
