5f04084eeefd6247223a23487de18bc70a97c6b0
baldx <mr.ahmad.m2006@gmail.com>
2024-03-23 21:34:11 +0100

    add footer

--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
 import useImageURL from "./Fetching";
 import Card from "./Card";
+import Footer from "./Footer";
 
 function ShoppingPage () {
 
     const { images, error, loading } = useImageURL();
 
-    if (error) return <p>A network error was encountered, {error}</p> //CHANGE IT LATER
-    if (loading) return <p>Loading...</p> //CHANGE IT LATER
+    if (error) return <p>A network error was encountered, {error}</p> 
+    if (loading) return <p>Loading...</p>
 
     function createCards () {
         let result = [];
@@ -24,6 +25,7 @@ function ShoppingPage () {
         <main className="shopping-main">
         {createCards()}
         </main>
+        <Footer />
         </>
     );
 }
