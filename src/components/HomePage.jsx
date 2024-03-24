e652cff1b9c3bc3dc8e14315edd3a8dbfadcde77
baldx <mr.ahmad.m2006@gmail.com>
2024-03-24 12:47:54 +0100

    import image

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import shoppingBackground from '../../assets/homepage-bg-img.jpg'
 
 function HomePage () {
 
     return (
         <>
         <main className="homepage-main">
-            <img src="./src/assets/homepage-bg-img.jpg" className="head-image" />
+            <img src={shoppingBackground} className="head-image" />
             <Link to="shopping" className="shop-btn">
                 <button>Shop now</button>
             </Link>
