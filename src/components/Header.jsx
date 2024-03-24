1b03fc0891f4a50e478e0ddba2f8f857a0f0095d
baldx <mr.ahmad.m2006@gmail.com>
2024-03-24 12:47:48 +0100

    add scroll behavior & import img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,24 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Shop } from "./router";
 import PropTypes from 'prop-types';
-
+import cartLogo from '../../assets/cart-logo.png';
+import cartIcon from '../../assets/cart.png';
 
 export default function Header ({ goBack }) { 
     const { cartItems, setShowShop } = useContext(Shop);
 
     function AboutMe () {
+
+        const scrollTo = (e) => {
+            e.preventDefault();
+            const targetElement = document.querySelector('footer');
+
+            if (targetElement) targetElement.scrollIntoView({behavior: "smooth"});
+        }
+        
         return (
             <>
-                <li><a href="#">About us</a></li>
+                <li><a href="#" onClick={scrollTo}>About us</a></li>
             </>
         )
     }
@@ -23,20 +32,21 @@ export default function Header ({ goBack }) {
         )
     }
 
+
     return (
         <>
             <header>
-                <div className="left-header">
-                    <img src="./src/assets/cart-logo.png" />
+                <Link to="/" className="left-header">
+                    <img src={cartLogo} />
                     <h1>Cartlify</h1>
-                </div>
+                </Link>
 
                     <ul className="right-header">
                         {goBack ? GoBack() : AboutMe()}
                         <li>
                             <Link to="../cart" onClick={() => setShowShop(true)} className="cart-container">
                                 Cart {cartItems.length}
-                                <img src="./src/assets/cart.png" className="cart" />
+                                <img src={cartIcon} className="cart" />
                             </Link>
                         </li>
                     </ul>
